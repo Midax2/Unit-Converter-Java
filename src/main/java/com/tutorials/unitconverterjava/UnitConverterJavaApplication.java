b5215b32919ccff7fe00d12795569ce7b6c5d7b4
Dmytro Dzhusov <dmytro.dzhusov@gmail.com>
2025-02-20 16:48:47 +0100

    Initial commit

---
+++ src/main/java/com/tutorials/unitconverterjava/UnitConverterJavaApplication.java
@@ -0,0 +1,13 @@
+package com.tutorials.unitconverterjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitConverterJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnitConverterJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tutorials/unitconverterjava/UnitConverterJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorials.unitconverterjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitConverterJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
